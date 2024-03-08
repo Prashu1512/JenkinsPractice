713c62578f84a58fb8bd8a34535cee2a47459377
Prashu1512 <prashanthi.rpem30@gmail.com>
2024-03-08 12:32:24 +0530

    "Octoban updated test scripts"

---
+++ src/test/java/jenkinsProject/Sample2Test.java
@@ -0,0 +1,17 @@
+package jenkinsProject;
+
+import org.testng.annotations.Test;
+
+public class Sample2Test 
+{
+	@Test(groups = "smoke")
+	public void tso3test()
+	{
+		System.out.println("----Test script03----");
+	}
+	@Test(groups = "regression")
+	public void tso4test()
+	{
+		System.out.println("----Test script04----");
+	}
+}
